3749dd3a0e87e2d55455b2686fb42db422c13e0b
leonchao <547128052@qq.com>
2021-04-23 16:59:43 +0800

    #log 1.Template Pattern.

--- DesignPattern.pro
+++ DesignPattern.pro
@@ -3,4 +3,5 @@ TEMPLATE = subdirs
 SUBDIRS += \
     FactoryPattern \
     AbstractFactoryPattern \
-    StrategyPattern
+    StrategyPattern \
+    TemplatePattern
---
+++ TemplatePattern/main.cpp
@@ -0,0 +1,46 @@
+﻿#include <QCoreApplication>
+
+class Game{
+public:
+    virtual void init() = 0;
+    virtual void startPlay() = 0;
+    virtual int endPlay() = 0;
+    void play() final{
+        init();
+        startPlay();
+        endPlay();
+    }
+};
+
+class BasketballGame : public Game{
+public:
+    virtual void init(){
+
+    }
+    virtual void startPlay(){
+
+    }
+    virtual int endPlay(){
+
+    }
+};
+
+class FootballGame : public Game{
+public:
+    virtual void init(){
+
+    }
+    virtual void startPlay(){
+
+    }
+    virtual int endPlay(){
+
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    return a.exec();
+}
